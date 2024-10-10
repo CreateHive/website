fb9279301707223a69f2fbdaaa267d2d9f11241b
Quales <27543795+Quales@users.noreply.github.com>
2024-10-10 13:59:56 +0200

    Added verify-code route

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Utils\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -45,5 +46,33 @@ class MainController extends AbstractController
 	public function discord(): Response
 	{
 		return $this->redirect('https://discord.gg/qr9QD9ang5');
+	}
+
+	#[Route('/reward/verify-code', name: 'app_reward_verif_code')]
+	public function verifyCode(Request $request): Response
+	{
+		// Récupérer le code envoyé via GET
+		$code = $request->query->get('code');
+		$secret = $request->query->get('secret');
+		if ($secret!= 'CF9DB9C626FBDA53DD13A43C4ECA6') {
+			echo 'invalid';
+			exit;
+		}
+
+		$filePath = __DIR__ . '/../../var/code_mois.txt';
+
+		if (!file_exists($filePath)) {
+			echo 'invalid';
+			exit;
+		}
+
+		$codeFromFile = trim(file_get_contents($filePath));
+
+		if ($code === $codeFromFile) {
+			echo 'valid';
+			exit;
+		}
+		echo 'invalid';
+		exit;
 	}
 }
